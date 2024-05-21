96a7a9e58e665ae06eb0b20db0d1ba3b07786502
Andrew Woosnam <AndrewT.Woosnam@gmail.com>
2024-05-20 21:21:41 -0400

    refactor: extract handleJoin

--- handlers/interactionHandler.js
+++ handlers/interactionHandler.js
@@ -1,6 +1,5 @@
 import { InteractionType, InteractionResponseType } from 'discord-interactions';
-import { joinVoiceChannel, VoiceConnectionStatus } from '@discordjs/voice';
-import { client } from '../bot.js'
+import { handleJoin } from './handleJoin.js';
 
 export async function handleInteraction(req, res) {
     try {
@@ -16,60 +15,27 @@ export async function handleInteraction(req, res) {
         if (type === InteractionType.APPLICATION_COMMAND) {
             const { name } = data;
 
-            switch (name) {
-                case "test":
-                    return res.send({
-                        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                        data: {
-                            content: 'hello world',
-                        },
-                    });
-
-                case "join":
-                    const guildId = req.body.guild_id;
-                    const guild = client.guilds.cache.get(guildId);
-                    const member = guild.members.cache.get(req.body.member.user.id);
-                    const channel = member.voice.channel;
-
-                    if (channel === null && channel === undefined) {
-                        return res.status(400).send({
-                            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                            data: {
-                                content: "You need to join a voice channel first!",
-                            },
-                        });
-                    }
-
-                    const connection = joinVoiceChannel({
-                        channelId: channel.id,
-                        guildId: guild.id,
-                        adapterCreator: guild.voiceAdapterCreator,
-                    });
-
-                    connection.on(VoiceConnectionStatus.Ready, () => {
-                        console.log('The bot has connected to the channel!');
-                    });
-
-                    connection.on(VoiceConnectionStatus.Disconnected, () => {
-                        console.log('Disconnected from the channel');
-                    });
-
-                    return res.send({
-                        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                        data: {
-                            content: 'Joined your voice channel!',
-                        },
-                    });
-
-                default:
-                    return res.status(400).send({
-                        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                        data: {
-                            content: 'Unhandled interaction type: ' + name,
-                        },
-                    });
+            if (name === "test") {
+                return res.send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        content: 'hello world',
+                    },
+                });
             }
 
+            else if (name === "join") {
+                return handleJoin(req, res);
+            }
+
+            else {
+                return res.status(400).send({
+                    type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        content: 'Unhandled interaction type: ' + name,
+                    },
+                });
+            }
         }
     } catch (error) {
         console.error(error);
