d3ba2135348f1679b8d7f7c633d1fd5e3e4f5999
Andrew Woosnam <AndrewT.Woosnam@gmail.com>
2024-05-20 20:45:07 -0400

    feat: connect to voice channel

--- handlers/interactionHandler.js
+++ handlers/interactionHandler.js
@@ -1,4 +1,6 @@
-import { InteractionType, InteractionResponseType, } from 'discord-interactions';
+import { InteractionType, InteractionResponseType } from 'discord-interactions';
+import { joinVoiceChannel, VoiceConnectionStatus } from '@discordjs/voice';
+import { client } from '../bot.js'
 
 export async function handleInteraction(req, res) {
     try {
@@ -20,6 +22,42 @@ export async function handleInteraction(req, res) {
                         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
                         data: {
                             content: 'hello world',
+                        },
+                    });
+
+                case "join":
+                    const guildId = req.body.guild_id;
+                    const guild = client.guilds.cache.get(guildId);
+                    const member = guild.members.cache.get(req.body.member.user.id);
+                    const channel = member.voice.channel;
+
+                    if (channel === null && channel === undefined) {
+                        return res.status(400).send({
+                            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                            data: {
+                                content: "You need to join a voice channel first!",
+                            },
+                        });
+                    }
+
+                    const connection = joinVoiceChannel({
+                        channelId: channel.id,
+                        guildId: guild.id,
+                        adapterCreator: guild.voiceAdapterCreator,
+                    });
+
+                    connection.on(VoiceConnectionStatus.Ready, () => {
+                        console.log('The bot has connected to the channel!');
+                    });
+
+                    connection.on(VoiceConnectionStatus.Disconnected, () => {
+                        console.log('Disconnected from the channel');
+                    });
+
+                    return res.send({
+                        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                        data: {
+                            content: 'Joined your voice channel!',
                         },
                     });
 
